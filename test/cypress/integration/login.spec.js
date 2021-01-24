96cdc999b38e72979538edf66d6b8666d30e40f2
Cristian F. Tovar <ctovar@abexuspr.com>
2021-01-24 18:57:54 -0500

    Aserciones

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -5,7 +5,7 @@ describe('Login tests', () => {
     cy.visit('/login')
   });
 
-  it('should sign up an user', ()=>{
+  it('should sign up a user', ()=>{
     cy.visit('/login')
     cy.contains('Crear una cuenta').click()
     cy.get('#name').type('User tester')
@@ -14,14 +14,25 @@ describe('Login tests', () => {
     cy.get('#password2').type('qwerty123')
     cy.contains('.button', 'Registrarse').click()
     cy.wait(3000)
+    cy.get('.error-msg').should('not.exist')
   });
 
-  it('should sign in an user', ()=>{
+  it('Must fail the sign in with a non-registered-user', ()=>{
+    cy.visit('/login')
+    cy.get('#email1').type('non-siggned@user.io')
+    cy.get('#password1').type('qwerty123')
+    cy.contains('.button', 'Ingresar').click()
+    cy.wait(3000)
+    cy.get('.error-msg').should('be.visible')
+  });
+
+  it('must login a user', ()=>{
     cy.visit('/login')
     cy.get('#email1').type('test@test.io')
     cy.get('#password1').type('qwerty123')
     cy.contains('.button', 'Ingresar').click()
     cy.wait(3000)
-  })
+    cy.contains('a', 'Dashboard').should('be.visible')
+  });
 })
 

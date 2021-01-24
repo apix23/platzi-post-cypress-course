098abe6dfeaa9492c63c326544cb250b143321ae
Cristian F. Tovar <ctovar@abexuspr.com>
2021-01-24 18:39:27 -0500

    Interactuando con elementos

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,7 +1,27 @@
 'use strict'
 
-describe('Pruebas del login', () => {
-    it('Debe cargar el login', ()  => {
-      cy.visit('/login')
-    })
+describe('Login tests', () => {
+  it('Should yield the login page', ()  => {
+    cy.visit('/login')
+  });
+
+  it('should sign up an user', ()=>{
+    cy.visit('/login')
+    cy.contains('Crear una cuenta').click()
+    cy.get('#name').type('User tester')
+    cy.get('#title').type('Company tester')
+    cy.get('#email2').type('test@test.io')
+    cy.get('#password2').type('qwerty123')
+    cy.contains('.button', 'Registrarse').click()
+    cy.wait(3000)
+  });
+
+  it('should sign in an user', ()=>{
+    cy.visit('/login')
+    cy.get('#email1').type('test@test.io')
+    cy.get('#password1').type('qwerty123')
+    cy.contains('.button', 'Ingresar').click()
+    cy.wait(3000)
   })
+})
+

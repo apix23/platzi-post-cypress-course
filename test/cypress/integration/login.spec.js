cb3851f593bf1dd715dbd3813766273928a269e8
Cristian F. Tovar <ctovar@abexuspr.com>
2021-01-24 19:14:44 -0500

    Hooks

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,12 +1,13 @@
 'use strict'
 
 describe('Login tests', () => {
-  it('Should yield the login page', ()  => {
-    cy.visit('/login')
-  });
 
-  it('should sign up a user', ()=>{
+  beforeEach(() => {
     cy.visit('/login')
+    cy.contains('h1', 'Bienvenido').should('be.visible')
+  })
+
+  it.skip('should sign up a user', () => {
     cy.contains('Crear una cuenta').click()
     cy.get('#name').type('User tester')
     cy.get('#title').type('Company tester')
@@ -17,8 +18,7 @@ describe('Login tests', () => {
     cy.get('.error-msg').should('not.exist')
   });
 
-  it('Must fail the sign in with a non-registered-user', ()=>{
-    cy.visit('/login')
+  it('Must fail the sign in with a non-registered-user', () => {
     cy.get('#email1').type('non-siggned@user.io')
     cy.get('#password1').type('qwerty123')
     cy.contains('.button', 'Ingresar').click()
@@ -26,13 +26,16 @@ describe('Login tests', () => {
     cy.get('.error-msg').should('be.visible')
   });
 
-  it('must login a user', ()=>{
-    cy.visit('/login')
+  it('must login a user', () => {
     cy.get('#email1').type('test@test.io')
     cy.get('#password1').type('qwerty123')
     cy.contains('.button', 'Ingresar').click()
     cy.wait(3000)
     cy.contains('a', 'Dashboard').should('be.visible')
   });
+
+  after(() => {
+    cy.log('All tests done')
+  })
 })
 

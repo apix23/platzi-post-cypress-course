92da728d1d64c943d3c891c1521042eae246db03
Cristian F. Tovar <ctovar@abexuspr.com>
2021-01-24 19:32:31 -0500

    Variables, Fixtures y Alias

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -3,19 +3,22 @@
 describe('Login tests', () => {
 
   beforeEach(() => {
+    cy.fixture('user.json').as('userData')
     cy.visit('/login')
     cy.contains('h1', 'Bienvenido').should('be.visible')
   })
 
-  it.skip('should sign up a user', () => {
-    cy.contains('Crear una cuenta').click()
-    cy.get('#name').type('User tester')
-    cy.get('#title').type('Company tester')
-    cy.get('#email2').type('test@test.io')
-    cy.get('#password2').type('qwerty123')
-    cy.contains('.button', 'Registrarse').click()
-    cy.wait(3000)
-    cy.get('.error-msg').should('not.exist')
+  it('should sign up a user', () => {
+    cy.get('@userData').then((userData)=>{
+      cy.contains('Crear una cuenta').click()
+      cy.get('#name').type(userData.name)
+      cy.get('#title').type(userData.company)
+      cy.get('#email2').type(userData.email)
+      cy.get('#password2').type(userData.password)
+      cy.contains('.button', 'Registrarse').click()
+      cy.wait(3000)
+      cy.get('.error-msg').should('not.exist')
+    })
   });
 
   it('Must fail the sign in with a non-registered-user', () => {
@@ -27,11 +30,13 @@ describe('Login tests', () => {
   });
 
   it('must login a user', () => {
-    cy.get('#email1').type('test@test.io')
-    cy.get('#password1').type('qwerty123')
-    cy.contains('.button', 'Ingresar').click()
-    cy.wait(3000)
-    cy.contains('a', 'Dashboard').should('be.visible')
+    cy.get('@userData').then((userData)=>{
+      cy.get('#email1').type(userData.email)
+      cy.get('#password1').type(userData.password)
+      cy.contains('.button', 'Ingresar').click()
+      cy.wait(3000)
+      cy.contains('a', 'Dashboard').should('be.visible')
+    });
   });
 
   after(() => {
